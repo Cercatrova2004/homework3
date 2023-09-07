8712978e265fd996b08b50442254b3621bc64838
Cercatrova2004 <137152072+Cercatrova2004@users.noreply.github.com>
2023-09-07 23:36:43 +0800

    Add files via upload

---
+++ lifo/target/debug/deps/lifo-483ac973b8a8326d.d
@@ -0,0 +1,5 @@
+/home/cercatrova/lifo/target/debug/deps/lifo-483ac973b8a8326d: src/main.rs
+
+/home/cercatrova/lifo/target/debug/deps/lifo-483ac973b8a8326d.d: src/main.rs
+
+src/main.rs:
---
+++ lifo/target/debug/lifo.d
@@ -0,0 +1,1 @@
+/home/cercatrova/lifo/target/debug/lifo: /home/cercatrova/lifo/src/main.rs
